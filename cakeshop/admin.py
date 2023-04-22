54e6d0ac1a11a8ffb32ce1e20205bfa0eb1531af
Trung <handsomezezo@gmail.com>
2023-04-22 14:31:48 +0700

    Update search admin

--- cakeshop/admin.py
+++ cakeshop/admin.py
@@ -2,8 +2,16 @@ from django.contrib import admin
 from .models import Category, Cake
 
 # Register your models here.
+
+
 class CakeAdmin(admin.ModelAdmin):
-    list_display = ('name', 'category', 'quantity','price', 'image_url')
-    
-admin.site.register(Category)
+    list_display = ('name', 'category', 'quantity', 'price', 'image_url')
+    search_fields = ('name', )
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
+
+
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Cake, CakeAdmin)
